132adc0a63c2fcc6d8d31f3490ccd9884382672a
Ubuntu <ubuntu@ip-172-31-37-208.ec2.internal>
2016-07-26 03:42:16 +0000

    Adds the rest of hw

--- emailApp.js
+++ emailApp.js
@@ -6,13 +6,12 @@ var email = {
       firstName: 'Priyam',
       lastName: 'Sarma',
       login:'priyam.sarma',
-      email: 'priyam.sarma@outlook.com'},
+      uid: 'priyam.sarma@outlook.com'},
     domainValues: {
       contactType:['work','friend','family'],
       phoneType:['mobile','home','work'],
       priorty:['High','Medium','Low'],
       taskStatus:['Not Started','In Progress','Complete'],
-      emailStatus:['Not Started','In Progress','Complete'],
       attending:[-1,0,1]
     },
     contacts: {
@@ -52,20 +51,20 @@ var email = {
     },
     mailBoxes: {
       inbox: {
-        conversationId1: {
+        id1: {
           from: 'priyam.sarma@outlook.com',
           to: 'mom@outlook.com',
           cc: '',
           subject: 'trash?',
           content: 'What do we do about all this trash?',
           size: 4000,
-          dateTime: new Date(2016, 0, 1, 2, 3, 4, 567), //new Date(year, month [, day, hours, minutes, seconds, ms])
+          dateTime: new Date(2016, 0, 1, 2, 1, 4, 567), //new Date(year, month [, day, hours, minutes, seconds, ms])
           importance: 'Very',
           read: true,
           category: 'Yellow',
-          emailStatus: 'Sent'
+          emailStatus: ''
         },
-        conversationId1: {
+        id2: {
           from: 'mom@outlook.com',
           to: 'priyam.sarma@outlook.com',
           cc: '',
@@ -76,48 +75,48 @@ var email = {
           importance: 'Very',
           read: true,
           category: 'Yellow',
-          emailStatus: 'Received'
+          emailStatus: ''
         },
-        conversationId1: {
+        id3: {
           from: 'priyam.sarma@outlook.com',
           to: 'mom@outlook.com',
           cc: '',
           subject: 'trash?',
           content: 'No I will not',
           size: 4000,
-          dateTime: new Date(2016, 0, 1, 2, 3, 4, 567), //new Date(year, month [, day, hours, minutes, seconds, ms])
+          dateTime: new Date(2016, 0, 1, 2, 10, 4, 567), //new Date(year, month [, day, hours, minutes, seconds, ms])
           importance: 'Very',
           read: true,
           category: 'Yellow',
-          emailStatus: 'Sent'
+          emailStatus: ''
         },
-        conversationId2: {
+        id4: {
           from: 'priyam.sarma@outlook.com',
           to: 'mom@outlook.com',
           cc: '',
           subject: 'Whats up with dad',
           content: 'Is He serious',
           size: 4000,
-          dateTime: new Date(2016, 0, 1, 2, 3, 4, 567), //new Date(year, month [, day, hours, minutes, seconds, ms])
+          dateTime: new Date(2016, 0, 1, 5, 3, 4, 567), //new Date(year, month [, day, hours, minutes, seconds, ms])
           importance: 'Very',
           read: true,
           category: 'Yellow',
-          emailStatus: 'Sent'
+          emailStatus: ''
         }
       },
       deleted: {
-        conversationId3: {
+        id5: {
           from: 'everyone.else@outlook.com',
           to: 'priyam.sarma@outlook.com',
           cc: '',
           subject: 'Spam Much',
           content: 'Click on this tasty spam',
           size: 2000,
-          dateTime: new Date(2016, 0, 1, 2, 3, 4, 567), //new Date(year, month [, day, hours, minutes, seconds, ms])
+          dateTime: new Date(2016, 0, 1, 1, 3, 4, 567), //new Date(year, month [, day, hours, minutes, seconds, ms])
           importance: 'Very',
           read: true,
           category: 'Yellow',
-          emailStatus: 'Received'
+          emailStatus: 'Deleted'
         }
       }
     }
@@ -137,5 +136,34 @@ for (var i = 0; i < inboxNames.length; i++) {
 }
 console.log(emails)
 //  Get the text of the second email in the visible list
+inboxEmails = Object.keys(email.mailBoxes.inbox)
+console.log(email.mailBoxes.inbox[inboxEmails[1]].content)
+
 //  Mark an email as sent
+for (var i = 0; i < inboxEmails.length; i++) {
+  if (email.mailBoxes.inbox[inboxEmails[i]].from === email.account.uid && email.mailBoxes.inbox[inboxEmails[i]].read ===undefined){
+    email.mailBoxes.inbox[inboxEmails[i]].emailStatus = 'Draft'
+  } else if (email.mailBoxes.inbox[inboxEmails[i]].from !== email.account.uid) {
+    email.mailBoxes.inbox[inboxEmails[i]].emailStatus = 'Received'
+  } else {
+    email.mailBoxes.inbox[inboxEmails[i]].emailStatus = 'Sent'
+  }
+}
+console.log(email)
 //  Add a draft email to the drafts mailbox
+// At this point, draft is a view of inbox for me
+email.mailBoxes.inbox.id6 = {
+  from: 'priyam.sarma@outlook.com',
+  to: 'mom@outlook.com',
+  cc: '',
+  subject: 'whasssuuupp',
+  content: 'whaahahassusup',
+  size: 4000,
+  dateTime: new Date(2016, 0, 8, 5, 3, 4, 567), //new Date(year, month [, day, hours, minutes, seconds, ms])
+  importance: 'Low',
+  read: undefined,
+  category: 'Yellow',
+  emailStatus: 'Draft'
+}
+
+console.log(email)
